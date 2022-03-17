49c3f6157803bb508548244530e404d0b63b9e90
Błażej Sowa <bsowa123@gmail.com>
2022-03-17 14:48:13 +0100

    Allow providing statically allocated memory in circular buffer

--- circular_buffer.hpp
+++ circular_buffer.hpp
@@ -4,13 +4,16 @@
 
 template <class T>
 class CircularBuffer {
-  T *values_;
+  T* values_;
   size_t size_;
   size_t iter_;
 
  public:
   explicit CircularBuffer(size_t size)
       : values_(new T[size]()), size_(size), iter_(0) {}
+
+  CircularBuffer(size_t size, T* buffer)
+      : values_(buffer), size_(size), iter_(0) {}
 
   T push_back(T val) {
     T tmp = values_[iter_];
