5f9648055b3b2e753c1c29fb640d8c67cece8687
Aleksander Szymański <bitterisland6@gmail.com>
2024-01-05 08:41:37 +0100

    added flag to prevent deleting pointer to static object
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- include/diff_drive_lib/circular_buffer.hpp
+++ include/diff_drive_lib/circular_buffer.hpp
@@ -9,16 +9,18 @@ class CircularBuffer {
   T* values_;
   size_t size_;
   size_t iter_;
+  std::atomic_bool dynamicAlloc_;
 
  public:
   explicit CircularBuffer(size_t size)
-      : values_(new T[size]()), size_(size), iter_(0) {}
+      : values_(new T[size]()), size_(size), iter_(0), dynamicAlloc_(true) {}
 
   CircularBuffer(size_t size, T* buffer)
-      : values_(buffer), size_(size), iter_(0) {}
+      : values_(buffer), size_(size), iter_(0), dynamicAlloc_(false) {}
 
   ~CircularBuffer() {
-    delete values_;
+    if (dynamicAlloc_.load())
+      delete values_;
   }
 
   T push_back(T val) {
