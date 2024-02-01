f288b1c7c1225844dcf30f7ca2a1a0ade1434f16
Aleksander Szymański <bitterisland6@gmail.com>
2024-02-01 22:37:44 +0100

    Fix memory leak in CircularBuffer class (#3)
    * Added constructor with deallocation of the pointer
    
    * added flag to prevent deleting pointer to static object
    
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>
    
    * add missing include
    
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>
    
    * code review guidelines
    
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>
    
    * update flag name
    
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>
    
    ---------
    
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- include/diff_drive_lib/circular_buffer.hpp
+++ include/diff_drive_lib/circular_buffer.hpp
@@ -9,13 +9,19 @@ class CircularBuffer {
   T* values_;
   size_t size_;
   size_t iter_;
+  const bool dynamic_alloc_;
 
  public:
   explicit CircularBuffer(size_t size)
-      : values_(new T[size]()), size_(size), iter_(0) {}
+      : values_(new T[size]()), size_(size), iter_(0), dynamic_alloc_(true) {}
 
   CircularBuffer(size_t size, T* buffer)
-      : values_(buffer), size_(size), iter_(0) {}
+      : values_(buffer), size_(size), iter_(0), dynamic_alloc_(false) {}
+
+  ~CircularBuffer() {
+    if (dynamic_alloc_)
+      delete values_;
+  }
 
   T push_back(T val) {
     T tmp = values_[iter_];
